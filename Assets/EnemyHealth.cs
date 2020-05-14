1f06a71f940313ded666648ae1222bca1e865bc4
Nikita Dmitriev <nikdmit8@gmail.com>
2020-05-14 16:41:56 +0300

    Raycasting, shooting, enemy health

---
+++ Assets/EnemyHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] int hitPoints = 100;
+
+    public void TakeDamage(int damage)
+    {
+        hitPoints -= damage;
+
+        if (hitPoints <= 0)
+        {
+            EnemyDeathSequence();
+        }
+    }
+
+    private void EnemyDeathSequence()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPSCamera;
+    [SerializeField] float shootRange = 100f;
+    [SerializeField] int damage = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPSCamera.transform.position, FPSCamera.transform.forward, out hit, shootRange))
+        {
+            Debug.Log("I hit" + hit.transform.name);
+
+            EnemyHealth enemyHealth = hit.transform.GetComponent<EnemyHealth>();
+            if (enemyHealth)
+            {
+                enemyHealth.TakeDamage(damage);
+            }
+        }
+        else
+        {
+            return;
+        }
+    }
+}
