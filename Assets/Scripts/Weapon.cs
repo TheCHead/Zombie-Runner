7029df8cb2d0561e394291a23bc429a09b874357
Nikita Dmitriev <nikdmit8@gmail.com>
2020-05-18 16:28:57 +0300

    Added fire and hit particle effects

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +8,9 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPSCamera;
     [SerializeField] float shootRange = 100f;
     [SerializeField] int damage = 10;
-
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitSparks;
+ 
     // Update is called once per frame
     void Update()
     {
@@ -18,10 +21,25 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
         if (Physics.Raycast(FPSCamera.transform.position, FPSCamera.transform.forward, out hit, shootRange))
         {
             Debug.Log("I hit" + hit.transform.name);
+
+            PlayHitParticles(hit);
+            
+
 
             EnemyHealth enemyHealth = hit.transform.GetComponent<EnemyHealth>();
             if (enemyHealth)
@@ -33,5 +51,11 @@ public class Weapon : MonoBehaviour
         {
             return;
         }
+    }
+
+    private void PlayHitParticles(RaycastHit hit)
+    {
+        GameObject hitEffect = Instantiate(hitSparks, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(hitEffect, 0.2f);
     }
 }
